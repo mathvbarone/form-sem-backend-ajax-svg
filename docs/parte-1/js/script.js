86e7257fb8142e2d9385b0ef3e6be200d48990c6
Matheus Barone <matheusvbarone@gmail.com>
2017-11-01 09:02:49 -0200

    Criando a estrutura inicial do Javascript

---
+++ docs/parte-1/js/script.js
@@ -0,0 +1,21 @@
+const startForm = {
+    // UI DECLARATION
+    ui: {
+    },
+
+    // FUNCTIONS
+    functions: {
+        formValidation: () => {
+        }
+
+    },
+
+    //EVENTS
+    events: {
+        init: () => {
+        }
+    }
+};
+
+//INIT FUNCTION
+startForm.events.init();
