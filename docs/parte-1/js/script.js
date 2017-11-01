ae1034768c37003f02a2552eb029900aa2612d50
Matheus Barone <matheusvbarone@gmail.com>
2017-11-01 09:10:21 -0200

    Criando validação simples, de campos vazios

--- docs/parte-1/js/script.js
+++ docs/parte-1/js/script.js
@@ -8,12 +8,24 @@ const startForm = {
     // FUNCTIONS
     functions: {
         formValidation: () => {
+            const fields = startForm.ui.fields;
+            const button = startForm.ui.button;
+
+            fields.forEach( field => {
+                field.value.length === 0 ? button.disabled = true : button.disabled = false;
+            });
         }
     },
 
     //EVENTS
     events: {
         init: () => {
+            const initUi =  startForm.ui;
+            const initFunctions =  startForm.functions;
+
+            initUi.fields.forEach( field => {
+                field.addEventListener("input", initFunctions.formValidation);
+            });
         }
     }
 };
